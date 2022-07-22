73b76a129e5f395b224579023c844e049cd04eff
AdrianDieDose <adrianduda2019@gmail.com>
2022-07-22 04:14:53 +0200

    Test

---
+++ .vscode-server/data/User/History/3e4350d7/H47E.js
@@ -0,0 +1,5 @@
+//Toast may be buggy: https://portal-vue.linusb.org/guide/getting-started.html#what-is-portalvue
+import { createApp } from "vue";
+import App from "./App.vue";
+
+createApp(App).mount("#app");
